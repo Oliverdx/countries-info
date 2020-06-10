be5d2864d57db89d781e345c2fc70fcf92f098d9
Vanderlei Oliveira <vanderlei327@gmail.com>
2020-06-10 01:39:01 -0300

    Header layout with dark mode

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -4,14 +4,15 @@ import './style.scss';
 export default function Header({ mode, changeMode }) {
 
   const [appMode, setAppMode] = useState(mode);
+
   useEffect(() => {
     setAppMode(mode);
   }, [mode]);
 
   return (
-    <header>
-      <h1>Where in the world? </h1>
-      <button onClick={(mode) => changeMode(mode)}>{appMode} Mode</button>
+    <header className="site-header">
+      <h1 className="site-title">Where in the world? </h1>
+      <button className="change-mode" onClick={(mode) => changeMode(mode)}>{appMode} Mode</button>
     </header>
   );
 
