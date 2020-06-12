a3584e0216f9a52af34b4f76ca0d71165a2819b8
Vanderlei Oliveira <vanderlei327@gmail.com>
2020-06-12 00:02:38 -0300

    minor fixes

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -3,17 +3,14 @@ import './style.scss';
 
 export default function Header({ mode, changeMode }) {
 
-  const [appMode, setAppMode] = useState(mode);
-
-  useEffect(() => {
-    setAppMode(mode);
-  }, [mode]);
-
   return (
     <header className="site-header">
       <div className="wrapper">
         <h1 className="site-title">Where in the world? </h1>
-        <button className="change-mode" onClick={(mode) => changeMode(mode)}>{appMode} Mode</button>
+        <button className="change-mode"
+          onClick={(mode) => changeMode(mode)}>
+          {mode === 'light' ? 'dark' : 'light'} Mode
+        </button>
       </div>
     </header>
   );
